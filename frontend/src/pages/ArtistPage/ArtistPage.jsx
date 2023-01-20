d0c22a92511a85e04d165f193a40d0b912b237dd
George Kuether <gwkuether@gmail.com>
2023-01-20 16:55:21 -0600

    Added Axios Call to Pass in Artist Name In Order to Retrieve Artist ID + Began Adding Information from Spotify API to populate Artist Page

--- frontend/src/pages/ArtistPage/ArtistPage.jsx
+++ frontend/src/pages/ArtistPage/ArtistPage.jsx
@@ -1,30 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 const ArtistPage = (props) => {
+  const state = useLocation();
+  const performer = state.state;
+  const [performerId, setPerformerId] = useState("");
+  const [monthlyListeners, setMonthlyListeners] = useState("")
+  const [artistBio, setArtistBio] = useState("")
+  const [topTracks, setTopTracks] = useState([])
 
-    const state = useLocation();
-    const performer = state.state
-    console.log(performer)
+  useEffect(() => {
+    fetchPerformerId();
 
-    useEffect(() => {
-        fetchPerformer();
-      }, []);
+  }, []);
 
-    
-      const fetchPerformer = async () => {
-        let response = await axios.get(`https://spotify23.p.rapidapi.com/search/?q=${performer}&type=artists&offset=0&limit=1&numberOfTopResults=1&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`)
-        debugger
-        console.log(response.data.artists.items[0]?.data.uri)
-      }
+//   const fetchPerformerId = async () => {
+//     let response = await axios.get(
+//       `https://spotify23.p.rapidapi.com/search/?q=${performer}&type=artists&offset=0&limit=1&numberOfTopResults=1&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
+    );
+    let text = response.data.artists?.items[0]?.data.uri;
+    const idArray = text.split("");
+    idArray.splice(0, 15);
+    let id = idArray.join("")
+    console.log(id)
+    setPerformerId(idArray.join(""));
+    fetchPerformerOverview(id)
+  };
 
+//   const fetchPerformerOverview = async (id) => {
+//     let response = await axios.get(
+//       `https://spotify23.p.rapidapi.com/artist_overview/?id=${id}&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
+//     );
+    console.log(response.data)
+    setMonthlyListeners(response.data.data.artist.stats.monthlyListeners)
+    debugger
+    console.log(response.data.data.artist.discography.topTracks.items.map(el => el.name))
+    setTopTracks(response.data.data.artist.discography.topTracks.items.map(el => el.name))
+    setArtistBio(response.data.data.artist.profile?.biography.text);
+  };
 
+  return (
+    <div>
+      <h1>This right here? You guessed it. It's the Artist Page!</h1>
+      <h1>Who's that artist? it's {performer}!</h1>
+      <p>here are the monthly listeners! {monthlyListeners}</p>
+      <p>and of course, the top tracks: {topTracks}</p>
+      <p>{artistBio}</p>
+    </div>
+  );
+};
 
-
-    return ( 
-        <h1>This right here? You guessed it. It's the Artist Page!</h1>
-     );
-}
- 
 export default ArtistPage;
