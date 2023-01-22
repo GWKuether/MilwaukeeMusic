63a28cc08f3634525dc63d7d4637926834b61874
George Kuether <gwkuether@gmail.com>
2023-01-22 14:37:30 -0600

    Added a Video Player Component that Pulls From Youtube's API to Search for an Artist's Name and Top Track

---
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const VideoPlayer = (props) => {
+  const [videoId, setVideoId] = useState("");
+
+  useEffect(() => {
+    fetchVideoId();
+  }, []);
+
+  const fetchVideoId = async () => {
+    console.log(props.searchInput);
+    // let response = await axios.get(
+    //   `https://www.googleapis.com/youtube/v3/search?q=${props.performer} ${props.topTrack}&key=AIzaSyADwfFOa8oCMWoWfKuESBZmEFDTEd8mB18&part=snippet&maxResults=1`
+    // );
+    console.log(response.data);
+    setVideoId(response.data.items[0].id.videoId);
+  };
+
+  return (
+    <div>
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        // src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoPlayer;
--- frontend/src/pages/ArtistPage/ArtistPage.jsx
+++ frontend/src/pages/ArtistPage/ArtistPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const ArtistPage = (props) => {
   const state = useLocation();
@@ -8,17 +9,17 @@ const ArtistPage = (props) => {
   const [performerId, setPerformerId] = useState("");
   const [monthlyListeners, setMonthlyListeners] = useState("")
   const [artistBio, setArtistBio] = useState("")
+  const [topTrack, setTopTrack] = useState("")
   const [topTracks, setTopTracks] = useState([])
 
   useEffect(() => {
     fetchPerformerId();
-
   }, []);
 
-//   const fetchPerformerId = async () => {
-//     let response = await axios.get(
-//       `https://spotify23.p.rapidapi.com/search/?q=${performer}&type=artists&offset=0&limit=1&numberOfTopResults=1&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
-    );
+  const fetchPerformerId = async () => {
+    // let response = await axios.get(
+    //   `https://spotify23.p.rapidapi.com/search/?q=${performer}&type=artists&offset=0&limit=1&numberOfTopResults=1&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
+    // );
     let text = response.data.artists?.items[0]?.data.uri;
     const idArray = text.split("");
     idArray.splice(0, 15);
@@ -28,15 +29,14 @@ const ArtistPage = (props) => {
     fetchPerformerOverview(id)
   };
 
-//   const fetchPerformerOverview = async (id) => {
-//     let response = await axios.get(
-//       `https://spotify23.p.rapidapi.com/artist_overview/?id=${id}&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
-//     );
+  const fetchPerformerOverview = async (id) => {
+    // let response = await axios.get(
+    //   `https://spotify23.p.rapidapi.com/artist_overview/?id=${id}&rapidapi-key=e4a27c7a77msh429e0aa2416efe0p168c02jsnfd69a5e83ce3`
+    // );
     console.log(response.data)
     setMonthlyListeners(response.data.data.artist.stats.monthlyListeners)
-    debugger
-    console.log(response.data.data.artist.discography.topTracks.items.map(el => el.name))
-    setTopTracks(response.data.data.artist.discography.topTracks.items.map(el => el.name))
+    console.log(response.data.data.artist.discography.topTracks.items[0].track.name)
+    setTopTrack(response.data.data.artist.discography.topTracks.items[0].track.name)
     setArtistBio(response.data.data.artist.profile?.biography.text);
   };
 
@@ -47,6 +47,7 @@ const ArtistPage = (props) => {
       <p>here are the monthly listeners! {monthlyListeners}</p>
       <p>and of course, the top tracks: {topTracks}</p>
       <p>{artistBio}</p>
+      <VideoPlayer performer={performer} topTrack={topTrack} />
     </div>
   );
 };
